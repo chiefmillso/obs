2b3e28309d212fafc8aba268a9eb413a16515681
David Miller <david.miller@obs.com.au>
2013-07-17 13:06:46 +1000

    added search provisioning scripts

--- OBS.Community.PowerShell/SP2013/ServiceApplications.ps1
+++ OBS.Community.PowerShell/SP2013/ServiceApplications.ps1
@@ -1,9 +1,12 @@
+$serverName = "MILLERD2013"
+
 # Ensure Timer Services are Running
 NET START SPAdminV4
 NET START SPTimerV4
 
 # Create Isolated App Domain
 Set-SPAppDomain "app.dev.local"
+
 
 $appPoolSubSvc = Get-SPServiceApplicationPool "Service Applications"
 
@@ -19,3 +22,25 @@ $proxyAppSvc = New-SPAppManagementServiceApplicationProxy -ServiceApplication $a
 
 # Set Application Management Tenancy
 Set-SPAppSiteSubscriptionName -Name "app" -Confirm:$false
+
+# Configure Search
+$searchPoolSvc = Get-SPServiceApplicationPool "Search Service Application"
+
+# Ensure Services
+Start-SPEnterpriseSearchServiceInstance $serverName
+Start-SPEnterpriseSearchQueryAndSiteSettingsServiceInstance $serverName
+
+# Provision Search Service Application
+$appSearchSvc = New-SPEnterpriseSearchServiceApplication -Partitioned -Name "SearchServiceApp" -ApplicationPool $searchPoolSvc -DatabaseName "SP-Dev-Search"
+$proxySearchSvc = New-SPEnterpriseSearchServiceApplicationProxy -Partitioned -Name "SearchServiceApp Proxy" -SearchApplication $appSearchSvc
+
+# "Configuring Search Component Topology..."
+$clone = $appSearchSvc.ActiveTopology.Clone()
+$searchServiceInstance = Get-SPEnterpriseSearchServiceInstance
+New-SPEnterpriseSearchAdminComponent –SearchTopology $clone -SearchServiceInstance $searchServiceInstance
+New-SPEnterpriseSearchContentProcessingComponent –SearchTopology $clone -SearchServiceInstance $searchServiceInstance
+New-SPEnterpriseSearchAnalyticsProcessingComponent –SearchTopology $clone -SearchServiceInstance $searchServiceInstance
+New-SPEnterpriseSearchCrawlComponent –SearchTopology $clone -SearchServiceInstance $searchServiceInstance
+New-SPEnterpriseSearchIndexComponent –SearchTopology $clone -SearchServiceInstance $searchServiceInstance
+New-SPEnterpriseSearchQueryProcessingComponent –SearchTopology $clone -SearchServiceInstance $searchServiceInstance
+$clone.Activate()
